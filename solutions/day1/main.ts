9be7cfa3db0d53777c312a4f7ebacd4c15ca5dfd
William Rothman <wdrothman@gmail.com>
2024-12-05 21:03:20 +0100

    Auto stash before merge of "main" and "origin/main"

--- solutions/day1/main.ts
+++ solutions/day1/main.ts
@@ -1,3 +1,21 @@
-const input = await Deno.readTextFile(Deno.args[0]);
+const input = Deno.readTextFileSync(Deno.args[0]);
 
-console.log(input);
+const { left, right } = input
+    .split("\n")
+    .reduce((acc: { left: number[], right: number[] }, cur: string) => {
+        const matches: string[] = cur.match(/\d+/g) as string[];
+
+        return {
+            left: [...acc.left, Number(matches[0])],
+            right: [...acc.right, Number(matches[1])],
+        }
+    }, { left: [], right: [] });
+
+left.sort((a, b) => a - b);
+right.sort((a, b) => a - b);
+
+const distance = left.reduce((sum, cur, i) => sum + Math.abs(cur - right[i]), 0);
+console.log(`Distance: ${distance}`);
+
+const similarity = left.reduce((sum, cur) => sum + cur * right.filter(el => el === cur).length, 0);
+console.log(`Similarity: ${similarity}`);
